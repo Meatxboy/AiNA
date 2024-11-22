a9bb1bd3c6c5ac51d948d29d49fc7b7732f71810
Meatxboy <gruzdev.ilya.v@gmail.com>
2024-11-22 19:01:40 +0500

    Update test_aina.py

--- test_aina.py
+++ test_aina.py
@@ -7,23 +7,26 @@
 import unittest
 import aina
 
+#Программа должна работать с любым вводом в переменную sent
+sent = 'random sentence'
+
 class TestMain(unittest.TestCase):
 
     def test_accuracy_not_none(self):
-        acc = aina.Em_T_accuracy('random sentence')
+        acc = aina.Em_T_accuracy(sent)
         self.assertIsNotNone(acc)
 
     def test_accuracy_percent(self):
-        acc = aina.Em_T_accuracy('random sentence')
+        acc = aina.Em_T_accuracy(sent)
         self.assertTrue(acc >= 0 and acc <= 100)
 
     def test_input_not_none(self):
-        lbl = aina.Em_T_label('random sentence')
+        lbl = aina.Em_T_label(sent)
         self.assertIsNotNone(lbl)
 
     def test_good_bad_ugly(self):
-        lbl = aina.Em_T_label('random sentence')
+        lbl = aina.Em_T_label(sent)
         self.assertTrue(lbl == 'neutral' or lbl == 'positive' or lbl == 'negative')
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.aina()
